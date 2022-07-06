02bf0912a955876c1939da7a9a96846ec74c0e86
Agsu <agsu77@gmail.com>
2022-07-05 21:50:37 -0300

    Commit inicial

---
+++ src/main/java/agsu/springframework/pet_clinc/PetClincApplication.java
@@ -0,0 +1,13 @@
+package agsu.springframework.pet_clinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agsu/springframework/pet_clinc/PetClincApplicationTests.java
@@ -0,0 +1,13 @@
+package agsu.springframework.pet_clinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
